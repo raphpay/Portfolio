303b087a07d9d0799583cf461b63a7bdcc62922a
raphpay <raphpay.dev@gmail.com>
2025-07-08 12:56:48 +0400

    Adapt texts

--- src/ui/section/HeroSection.tsx
+++ src/ui/section/HeroSection.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AnimatedBackgroundShape from "../components/AnimatedBackgroundShape";
 
+const highlight = "font-bold text-fuchsia-500";
+
 const HeroSection: React.FC = () => {
   return (
-    <section className="relative w-full min-h-screen flex flex-col items-center justify-center overflow-hidden">
+    <section className="relative w-full min-h-screen flex flex-col items-center justify-center overflow-hidden px-2">
       <AnimatedBackgroundShape />
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center">
@@ -15,17 +17,29 @@ const HeroSection: React.FC = () => {
         </div>
         {/* Headline */}
         <h1 className="text-3xl md:text-5xl font-extrabold text-black text-center mb-4 leading-tight">
-          Bonjour, Je suis{" "}
-          <span className="text-fuchsia-400">Raphaël Payet</span>
-          ,<br />
-          un développeur Web & Mobile.
+          Bonjour,<br></br> je suis{" "}
+          <span className={highlight}>
+            Raphaël Payet,<br></br>développeur Web & Mobile
+          </span>
+          .
         </h1>
         {/* Description */}
+        <p className="text-lg md:text-xl text-gray-500 text-center max-w-xl font-medium mb-2">
+          Je conçois et développe des sites web modernes et des applications
+          mobiles{" "}
+          <span className={highlight}>
+            rapides, intuitives et centrées sur l'utilisateur
+          </span>
+          .
+        </p>
         <p className="text-lg md:text-xl text-gray-500 text-center max-w-xl font-medium">
-          Je designe et crée des sites-web modernes et des applications mobiles
-          qui sont rapides, réactives et orientées utilisateurs. Je me
-          spécialise dans le développement React pour le web, et
-          React-Native/SwiftUI pour le développement mobile.
+          <br></br>
+          Spécialisé en <span className={highlight}>React (web)</span>,{" "}
+          <span className={highlight}>React Native</span> et{" "}
+          <span className={highlight}>SwiftUI (mobile)</span>, j'allie{" "}
+          <span className={highlight}>design réfléchi</span> et{" "}
+          <span className={highlight}>performance technique</span> pour donner
+          vie à vos idées, avec énergie et précision.
         </p>
       </div>
     </section>
